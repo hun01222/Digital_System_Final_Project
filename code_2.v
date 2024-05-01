7c34ba5bb47dbd8e0d41674da47b5508efae6992
hun01222 <hun0122232@gmail.com>
2024-05-01 20:24:38 +0900

    add p2

---
+++ code_2.v
@@ -0,0 +1,53 @@
+module d_latch_module(
+  input d, clk,
+  output q, q_bar
+);
+
+  reg q, q_bar;
+  wire r, s;
+
+  assign s <= !(d && clk);
+  assign r <= !(s && clk);
+  assign q <= !(q_bar && s);
+  assign q_bar <= !(q && r);
+
+endmodule
+
+module d_flip_flop_module(
+  input d, clk,
+  output q, q_bar
+);
+
+  wire q_master, q_master_bar;
+
+  d_latch_module master(.d(d), .clk(!clk), .q(q_master), .q_bar(q_master_bar));
+  d_latch_module slave(.d(q_master), .clk(clk), .q(q), .q_bar(q_bar));
+
+endmodule
+
+module jk_flip_flop_module(
+  input j, k, rst, clk,
+  output q, q_bar
+);
+
+  wire d;
+
+  assign d = ((j&&q_bar)||(!k&&q));
+
+  d_flip_flop_module d_flip_flop(.d(d), .clk(clk), .q(q), .q_bar(q_bar));
+
+endmodule
+
+module alert_module(
+  input a, b, rst, clk,
+  output q, q_bar
+);
+
+  wire j, k;
+  
+  assign j = !(rst) && a && b;
+  assign k = !(!(rst) && a && b);
+
+  jk_flip_flop_module jk_flip_flop(.j(j), .k(k), .rst(rst), .clk(clk), .q(q), .q_bar(q_bar));
+
+endmodule
---
+++ tb_2.v
@@ -0,0 +1,32 @@
+`timescale 1ns/1ns
+
+module tb_2
+
+  reg A, B, RST, CLK;
+
+  reg Q, Q_BAR;
+
+  alert_module alert(.a(A), .b(B), .rst(RST), .clk(CLK), .q(Q), .q_bar(Q_BAR));
+
+  initial begin
+    CLK = 1'b0;
+  end
+
+  initial begin
+    forever begin
+      #5 CLK = !CLK;
+    end
+  end
+
+  initial begin
+    RST = 1'b0, A = 1'b0, B = 1'b0;
+    #10 RST = 1'b0, A = 1'b0, B = 1'b1;
+    #10 RST = 1'b0, A = 1'b1, B = 1'b0;
+    #10 RST = 1'b0, A = 1'b1, B = 1'b1;
+    #10 RST = 1'b1, A = 1'b0, B = 1'b0;
+    #10 RST = 1'b1, A = 1'b0, B = 1'b1;
+    #10 RST = 1'b1, A = 1'b1, B = 1'b0;
+    #10 RST = 1'b1, A = 1'b1, B = 1'b1;
+  end
+
+endmodule
