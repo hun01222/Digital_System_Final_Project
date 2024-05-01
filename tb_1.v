028799757c53d3e3d7f45798b87400679a163329
hun01222 <hun0122232@gmail.com>
2024-05-01 12:45:56 +0900

    fix tb

--- tb_1.v
+++ tb_1.v
@@ -14,7 +14,7 @@ module tb_1
   initial begin
     A = 4'b0001; B = 4'b0011; S = 0; // 1 + 3
     #10 A = 4'b0100; B = 4'b0010; S = 1; // 4 - 2
-    #10 A = 4'b0100; B = 4'b0010; S = 1; // 2 - 4
+    #10 A = 4'b0010; B = 4'b0100; S = 1; // 2 - 4
     #10 A = 4'b0100; B = 4'b0110; S = 0; // 4 + 6, overflow about addition
     #10 A = 4'b1100; B = 4'b1010; S = 0; // (-4) + (-2), overflow about subtraction
   end
