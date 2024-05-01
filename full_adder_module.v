929897e76894f507c76f4930199b0c9e9b734c00
hun01222 <hun0122232@gmail.com>
2024-05-01 12:37:48 +0900

    fix cout

--- full_adder_module.v
+++ full_adder_module.v
@@ -7,7 +7,7 @@ module full_adder_module(a, b, cin, sum, cout);
   half_adder_module HA1(.a(a), .b(b), .sum(sum1), .carry(cout1));
   half_adder_module HA2(.a(sum1), .b(cin), .sum(sum), .carry(cout2));
 
-  assign cout = cout1 + cout2;
+  assign cout <= cout1 + cout2;
 
 
 endmodule
