ced568e903f770ae5224e0fdb8adfa24f5cb6673
hun01222 <hun0122232@gmail.com>
2024-05-01 12:42:48 +0900

    merge

--- code_1.v
+++ code_1.v
@@ -1,3 +1,28 @@
+module half_adder_module(a, b, sum, carry);
+	input a, b;
+	output sum, carry;
+  reg sum, carry;
+	
+	always@(*)
+	begin
+		{carry, sum} = a + b;
+	end
+
+endmodule
+
+module full_adder_module(a, b, cin, sum, cout);
+	input a, b, cin;
+	output sum, cout;
+	wire sum1, cout1, cout2;
+	
+  half_adder_module HA1(.a(a), .b(b), .sum(sum1), .carry(cout1));
+  half_adder_module HA2(.a(sum1), .b(cin), .sum(sum), .carry(cout2));
+
+  assign cout = cout1 + cout2;
+
+
+endmodule
+
 module binary_adder_subtracter_module
 #(parameter width = 4)
 (
